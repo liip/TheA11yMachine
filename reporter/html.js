948ccb62ddb3d1ca6d93fcda6f9575ba844a8915
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-12-29 14:19:41 +0100

    Report is saved in a specific directory.

--- reporter/html.js
+++ reporter/html.js
@@ -1,8 +1,11 @@
 'use strict';
 
 var fs = require('fs');
+var mkdirp = require('mkdirp');
+var crypto = require('crypto');
 
 module.exports = {
+    config: config,
     begin: emptyFunction,
     error: reportError,
     debug: emptyFunction,
@@ -10,44 +13,80 @@ module.exports = {
     results: reportResults
 };
 
+var outputDirectory = null;
+var indexStream = null;
+
 function emptyFunction() {}
 
+function config(options) {
+    outputDirectory = options.outputDirectory;
+    mkdirp(outputDirectory);
+    indexStream = fs.createWriteStream(outputDirectory + '/index.html');
+}
+
 function reportError(message) {
-    console.error(message);
+    //console.error(message);
 }
 
 function reportResults(results, url) {
-    console.log(buildHtml(results, url));
+    var hash = crypto.createHash('sha1').update(url).digest('hex');
+    fs.writeFileSync(
+        outputDirectory + '/' + hash + '.html',
+        buildHtml(results, url),
+        {
+            encoding: 'utf8',
+            mode: 0o644,
+            flag: 'w'
+        }
+    );
+    fs.writeFileSync(
+        outputDirectory + '/index.html',
+        '<li><a href="./' + hash + '.html">' + url + '</a></li>',
+        {
+            encoding: 'utf8',
+            mode: 0o644,
+            flag: 'a'
+        }
+    );
 }
 
 function buildHtml(results, url) {
     var renderMain = template(__dirname + '/templates/report.html');
+
     return renderMain({
-        date: new Date(),
-        errorCount: results.filter(isError).length,
+        date        : new Date(),
+        errorCount  : results.filter(isError).length,
         warningCount: results.filter(isWarning).length,
-        noticeCount: results.filter(isNotice).length,
-        results: buildResultsHtml(results),
-        url: url
+        noticeCount : results.filter(isNotice).length,
+        results     : buildResultsHtml(results),
+        url         : url
     });
 }
 
 function buildResultsHtml(results) {
     var renderResult = template(__dirname + '/templates/result.html');
-    return results.map(function(result) {
-        result.typeLabel = upperCaseFirst(result.type);
-        return renderResult(result);
-    }).join('');
+
+    return results.map(
+        function(result) {
+            result.typeLabel = upperCaseFirst(result.type);
+
+            return renderResult(result);
+        }
+    ).join('');
 }
 
 function template(filePath) {
     var content = fs.readFileSync(filePath, 'utf-8');
+
     return function(context) {
         var output = content;
-        Object.keys(context).forEach(function(key) {
-            output = output.replace(new RegExp('\\{' + key + '\\}', 'g'), escapeHtml(context[key]));
-            output = output.replace(new RegExp('\\{' + key + '\\|raw\\}', 'g'), context[key]);
-        });
+        Object.keys(context).forEach(
+            function(key) {
+                output = output.replace(new RegExp('\\{' + key + '\\}', 'g'), escapeHtml(context[key]));
+                output = output.replace(new RegExp('\\{' + key + '\\|raw\\}', 'g'), context[key]);
+            }
+        );
+
         return output.replace(/\s+/g, ' ').trim();
     };
 }
